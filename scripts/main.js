19daa32d01acd4c3d5ccdd0d2934528539604615
Colin Brochard <colinbrochard@Colins-MacBook-Air.local>
2018-06-12 14:19:58 -0500

    revert IP to 18.219.7.103

--- scripts/main.js
+++ scripts/main.js
@@ -50,7 +50,7 @@ function getRoutes() {
 	resultsHeadingIsLoading();
 	clearRoutes();
 
-	fetch(`http://172.31.18.9`, {
+	fetch(`http://18.219.7.103`, { 
 		method: 'POST',
 		mode: 'cors',
 		body: JSON.stringify(getFormData()),
